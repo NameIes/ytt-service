cf827932756badb735aecdbb6edf2f75a48e809a
Nameless <nameies@yandex.ru>
2024-04-05 15:07:01 +0300

    Added test function.

--- db_models/urls.py
+++ db_models/urls.py
@@ -3,5 +3,6 @@ from db_models import views
 
 
 urlpatterns = [
+    path('send_post/', views.send),
     # path('', views.index),
 ]
--- db_models/views.py
+++ db_models/views.py
@@ -1,4 +1,11 @@
+import requests
+from django.http import HttpRequest
 from django.contrib.auth.decorators import login_required
+
+
+def send(request):
+    requests.post('https://crk-31.ru/api/post/', json={'test': 'test'})
+    return HttpRequest('Ok')
 
 
 @login_required()
